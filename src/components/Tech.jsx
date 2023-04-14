f5b3d47cc53bf4a857f4d857db9d0eeec4b1b709
Harry Suter <harry.suter.hs@gmail.com>
2023-04-14 11:30:30 +0300

    tech icons for phone screen update

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -3,15 +3,40 @@ import React from "react";
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
+import { useEffect, useState } from "react";
 
 const Tech = () => {
+  const [phoneScreen, setPhoneScreen] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth < 768) {
+      setPhoneScreen(true);
+    } else {
+      setPhoneScreen(false);
+    }
+  }, []);
   return (
     <div className="flex flex-row flex-wrap justify-center gap-10">
-      {technologies.map((technology) => (
-        <div className="w-28 h-28" key={technology.name}>
-          <BallCanvas icon={technology.icon} />
-        </div>
-      ))}
+      {phoneScreen ? (
+        <>
+          {technologies.map((technology) => (
+            <div
+              className="w-28 h-28 bg-white rounded-full p-2"
+              key={technology.name}
+            >
+              <img src={technology.icon} />
+            </div>
+          ))}
+        </>
+      ) : (
+        <>
+          {technologies.map((technology) => (
+            <div className="w-28 h-28 " key={technology.name}>
+              <BallCanvas icon={technology.icon} />
+            </div>
+          ))}
+        </>
+      )}
     </div>
   );
 };
